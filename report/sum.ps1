0a60e78be8e6b9685a4bacb0b981e2f3fd5cb34c
Adrian Biro <biroadrian@protonmail.com>
2023-01-13 09:12:28 +0100

    asd

--- report/sum.ps1
+++ report/sum.ps1
@@ -1,7 +1,8 @@
 $FilePath = ".\files\Clockify_Time_Report_Detailed_09_01_2023-15_01_2023.csv"
 #Decimal hours = hours + minutes/60 + seconds/3600
+#https://calculatordaily.com/decimal-hours-to-hours-minutes-calculator
 $a = foreach ($i in (Import-Csv $FilePath)) {$i } 
-$a | % {$sum = @{}} {
+$a | Foreach-Object  {$sum = @{}} {
     [double] $num = $_."Duration (decimal)"
     if ($sum[$_.Project]) {
         $sum[$_.Project] += $num
@@ -10,3 +11,11 @@ $a | % {$sum = @{}} {
         $sum[$_.Project] = $num
     }
 } {$sum}
+
+function get-hours-from-decimal{
+    param([double] $num)
+    [int] $hours = $num % 1
+    [int] $minutes = ($num * 60) % 60
+    [int] $seconds = ($num * 3600) % 60
+    return "{0}:{1}:{2}" -f $hours, $minutes, $seconds
+}
--- report/sum.sh
+++ report/sum.sh
@@ -1,11 +1,8 @@
- awk -F',' '{
-  arr[$1] += gsub("\"","", $15)
+ awk -F',' 'NR > 1{ 
+  gsub("\"","", $15)
+   arr[$1] += $15
 }
 END{for (i in arr) {
-      if(length(i)){
         print i ,arr[i]
       }
-   }
-}' files/Clockify_Time_Report_Detailed_09_01_2023-15_01_2023.csv
-#awk -F',' '{ print  $15}' files/Clockify_Time_Report_Detailed_09_01_2023-15_01_2023.csv
-# vráti string a pomieša columns
+}' ${1:-"files/Clockify_Time_Report_Detailed_09_01_2023-15_01_2023.csv"}
