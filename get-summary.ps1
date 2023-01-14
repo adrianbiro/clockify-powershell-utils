1e8fb93e34273936083c0cf1e05bc85398616cc9
Adrian Biro <biroadrian@protonmail.com>
2023-01-14 18:06:31 +0100

    draft is done plus some TODOes, rewrite in normal language still is needed

--- get-summary.ps1
+++ get-summary.ps1
@@ -2,19 +2,31 @@
 #https://clockify.me/developers-api#tag-Workspace
 #https://clockify.me/developers-api#tag-Reports
 #TODO https://stackoverflow.com/a/62617490 hash tables encoding problem switch to golang on python
-$token = Get-Content .config
-$workspacesJson = curl -s -H "X-Api-Key: $token" https://api.clockify.me/api/v1/workspaces
+param(
+  [string] $Start,
+  [string] $End,
+  [string] $Token
+)
+if((-not $Start) -or (-not $End)){
+  "Usage:`n`t{0}  -Start '2023-01-01' -End '2023-01-15'" -f $MyInvocation.MyCommand.Name
+  exit 1
+}
+if ((-not $Token) -and (Test-Path -Path ".config")){
+  $Token = Get-Content -Path ".config"
+} else {
+  "Specify token with -Token 'token_string'"
+  exit 1
+}
+$workspacesJson = curl -s -H "X-Api-Key: $Token" "https://api.clockify.me/api/v1/workspaces"
 $wpId = @{}; foreach ($i in ($workspacesJson | ConvertFrom-Json)) { $wpId[$i.Name] = $i.ID }
-function get-reports () {
+$jsonstring = '{"dateRangeStart": "' + $Start + 'T00:00:00.000Z", "dateRangeEnd": "' + $End + 'T23:59:59.000Z", "summaryFilter": { "groups": [ "PROJECT"]}}'
+
+function get-report () {
   param([string] $ws)
-  #detailed body $jsonstring = '{"dateRangeStart": "2023-01-10T00:00:00.000Z", "dateRangeEnd": "2023-01-13T00:00:00.000Z", "detailedFilter": { "page": 1, "pageSize": 50} }'
-  #curl -X POST -s -H "X-Api-Key: $token" -H "Content-Type: application/json" "https://reports.api.clockify.me/v1/workspaces/$ws/reports/detailed" -d $jsonstring
-  #TODO summary body dates from args
-  $jsonstring = '{"dateRangeStart": "2023-01-10T00:00:00.000Z", "dateRangeEnd": "2023-01-13T23:59:59.000Z", "summaryFilter": { "groups": [ "PROJECT"]}}'
   return $(curl -X POST -s -H "X-Api-Key: $token" -H "Content-Type: application/json" "https://reports.api.clockify.me/v1/workspaces/$ws/reports/summary" -d $jsonstring)
 }
 $AllReportsjson = @{}
-foreach ($ws in $wpId.GetEnumerator()) { $AllReportsjson[$ws.Name] = get-reports $($ws.Value) }
+foreach ($ws in $wpId.GetEnumerator()) { $AllReportsjson[$ws.Name] = get-report -ws $($ws.Value) }
 $sum = @{} 
 foreach ($i in $AllReportsjson.GetEnumerator()) {
   ($i.Value | ConvertFrom-Json).groupOne | Foreach-Object {
@@ -28,13 +40,12 @@ foreach ($i in $AllReportsjson.GetEnumerator()) {
     }
   } 
 }
-#$sum | ogv #Export-Csv -Path "zmaz.csv"
-#TODO report name with date from args for json body request 
-$reportPath = ".\summary.csv"
+#$sum | ogv; exit 
+$reportPath = ".\summary{0}_{1}.csv" -f $Start, $End
 New-Item -Path $reportPath -ErrorAction "SilentlyContinue" | Out-Null
-#TODO generuj csv z hash mapy cez custop obj
+#TODO make csv from hash map with custom obj. Like this `$sum | Export-Csv -Path "zmaz.csv"` but invertedlines to columns
 '"Project","Hours"' | Add-Content -Path $reportPath
 foreach ($i in $sum.GetEnumerator()) {
-  "`"{0}`",`"{1}`"" -f $i.Name, ([math]::Round(($i.Value / 3600),2)) | Add-Content -Path $reportPath
+  "`"{0}`",`"{1}`"" -f $i.Name, ([math]::Round(($i.Value / 3600), 2)) | Add-Content -Path $reportPath
 }
 
