68c801f542b9857b38caee900a6c0e91234b9323
Adrian Biro <biroadrian@protonmail.com>
2023-01-16 15:42:17 +0100

    run

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Make report from clockify
+tldr
+```pwsh
+PS C:\clockify> .\run.ps1 -Start '2023-01-01' -End '2023-01-15'
+```
+or
 ```pwsh
 PS C:\clockify> .\all-summary-csv.ps1 -Start '2023-01-01' -End '2023-01-15'
 PS C:\clockify> .\summary-for-each-workspace.ps1 -Start '2023-01-01' -End '2023-01-15'
---
+++ run.ps1
@@ -0,0 +1,19 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $start,
+    [Parameter(Mandatory=$true)]
+    [string] $End
+)
+.\all-summary-csv.ps1 -Start $Start -End $End
+.\summary-for-each-workspace.ps1 -Start $Start -End $End
+if(-not (test-path venv)) {
+    python3 -m venv venv
+    .\venv\Scripts\Activate.ps1
+    pip install -r requirements.txt
+}
+.\venv\Scripts\Activate.ps1
+Set-Location -Path "reports" 
+python ..\multiple2one.py
+Set-Location -Path "../"
+deactivate
+"Final report is in:`n`t{0}" -f (Join-path -Path $pwd.path -ChildPath ("\reports\{0}_{1}.xlsx" -f $Start, $End))
