442bcdbe491c5df2a6118c5efdf314952e125bad
Adrian Biro <biroadrian@protonmail.com>
2023-01-18 11:18:15 +0100

    add project in bulk, skip duplicates

---
+++ add-projects-in-bulk.ps1
@@ -0,0 +1,16 @@
+param(
+  [Parameter(Mandatory = $true)]
+  [string] $ClientName,
+  [Parameter(Mandatory = $true)]
+  [string] $NameOfWorkspace,
+  [Parameter(Mandatory = $true)]
+  [string] $File
+)
+$noadd = .\get-Projects.ps1 -NameOfWorkspace $NameOfWorkspace
+#$noadd = @("foo", "bar")
+foreach($i in Get-Content -Path $file) {
+    if($noadd -contains $i){ 
+        continue
+    }
+    .\add-Project.ps1 -NewProjectName $i -ClientName $ClientName -NameOfWorkspace $NameOfWorkspace
+}
---
+++ get-Projects.ps1
@@ -0,0 +1,35 @@
+﻿#https://clockify.me/developers-api
+param(
+  [Parameter(Mandatory = $true)]
+  [string] $NameOfWorkspace,
+  [string] $Token
+)
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+if ((-not $Token) -and (Test-Path -Path ".config")) {
+  $Token = Get-Content -Path ".config"
+}
+else {
+  "Specify token with -Token 'token_string'"
+  exit 1
+}
+$workspacesJson = curl -s -H "X-Api-Key: $Token" "https://api.clockify.me/api/v1/workspaces"
+$wpId = @{}; foreach ($i in ($workspacesJson | ConvertFrom-Json)) { $wpId[$i.Name] = $i.ID }
+
+function get-projects {
+  param([string] $ws)
+  $LoginParameters = @{ Uri = 'https://api.clockify.me/api/v1/workspaces/' + $ws + '/projects'
+    method                  = 'GET'
+    "Headers"               = @{"X-Api-Key" = $Token }
+  }
+  return ((Invoke-WebRequest @LoginParameters).Content | ConvertFrom-Json).name
+}
+
+function main {
+  foreach ($ws in $wpId.GetEnumerator()) { 
+    if ($ws.Name -eq $NameOfWorkspace) {
+      return get-projects -ws $ws.Value 
+    }
+  }
+}
+main
