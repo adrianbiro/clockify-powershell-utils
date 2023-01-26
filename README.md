c5643ec6c5141966776493a788e203940808b21e
Adrian Biro <biroadrian@protonmail.com>
2023-01-26 09:21:18 +0100

    detailed report per project for all workspaces

--- README.md
+++ README.md
@@ -15,3 +15,20 @@ final report is in `reports\2023-01-01_2023-01-15.xlsx`
 
 ## Excel stuff
 decimal time to hours: `=TEXT(SUM(C2:C56)/24,"[h]:mm:ss")` or `=TEXT(SUM(OFFSET($C$2,0,0,COUNTA(C:C),1))/24,"[h]:mm:ss")`
+
+# Make detailed report for each project from all workspaces
+```pwsh
+PS C:\clockify> ./detailed-for-project.ps1  -Start '2023-01-01' -End '2023-01-15'
+```
+
+# Change client name in all workspaces
+```pwsh
+PS C:\clockify> .\change-client-name-in-all-ws.ps1 -NewClientName 'Office&Facility Management' -ClientName 'Office Management'
+```
+
+# Add Project to workspace
+```pwsh
+PS C:\clockify> ./add-Project.ps1 -NameOfWorkspace 'Bar' -NewProjectName 'Foo' -ClientName 'Lol sro.'
+# or
+PS C:\clockify> ./add-Projects-in-bulk.ps1 -NameOfWorkspace 'Bar' -File 'Foo.txt' -ClientName 'Lol sro.'
+```
--- detailed-for-project.ps1
+++ detailed-for-project.ps1
@@ -2,68 +2,75 @@
 #https://clockify.me/developers-api#tag-Workspace
 #https://clockify.me/developers-api#tag-Reports
 param(
-  [string] $Start,
-  [string] $End,
-  [string] $Token
+    [string] $Start,
+    [string] $End,
+    [string] $Token
 )
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 if ((-not $Start) -or (-not $End)) {
-  "Usage:`n`t{0}  -Start '2023-01-01' -End '2023-01-15'" -f $MyInvocation.MyCommand.Name
-  exit 1
+    "Usage:`n`t{0}  -Start '2023-01-01' -End '2023-01-15'" -f $MyInvocation.MyCommand.Name
+    exit 1
 }
 if ((-not $Token) -and (Test-Path -Path ".config")) {
-  $Token = Get-Content -Path ".config"
+    $Token = Get-Content -Path ".config"
 }
 else {
-  "Specify token with -Token 'token_string'"
-  exit 1
+    "Specify token with -Token 'token_string'"
+    exit 1
 }
+$pathToReports = "projectreports"
+Remove-Item -Recurse -Force -ErrorAction "SilentlyContinue" -Path $pathToReports | Out-Null  
+mkdir $pathToReports -ErrorAction "SilentlyContinue" | Out-Null
 $workspacesJson = curl -s -H "X-Api-Key: $Token" "https://api.clockify.me/api/v1/workspaces"
 $wpId = @{}; foreach ($i in ($workspacesJson | ConvertFrom-Json)) { $wpId[$i.Name] = $i.ID }
 $jsonstring = @{
-  "dateRangeStart" = $Start + 'T00:00:00.000Z'
-  "dateRangeEnd" = $End + 'T23:59:59.000Z'
-  "detailedFilter" = @{
-    "page" = 1
-    "pageSize" = 50
-    "exportType" = "JSON"
-}
+    "dateRangeStart" = $Start + 'T00:00:00.000Z'
+    "dateRangeEnd"   = $End + 'T23:59:59.000Z'
+    "detailedFilter" = @{
+        "page"       = 1
+        "pageSize"   = 50
+        "exportType" = "JSON"
+    }
 } | ConvertTo-Json
 function get-report () {
-  param([string] $ws)
-  return $(curl -X POST -s -H "X-Api-Key: $token" -H "Content-Type: application/json" "https://reports.api.clockify.me/v1/workspaces/$ws/reports/detailed" -d $jsonstring)
+    param([string] $ws)
+    return $(curl -X POST -s -H "X-Api-Key: $token" -H "Content-Type: application/json" "https://reports.api.clockify.me/v1/workspaces/$ws/reports/detailed" -d $jsonstring)
 }
-# function get-hours-from-decimal {
-#   #Decimal hours = hours + minutes/60 + seconds/3600
-#   #https://calculatordaily.com/decimal-hours-to-hours-minutes-calculator
-#   param([double] $num)
-#   [int] $hours = [math]::Floor($num) # Floor to make it consistent with Clockify web app
-#   [int] $minutes = [math]::Floor(($num * 60) % 60)  
-#   [int] $seconds = [math]::Floor(($num * 3600) % 60)
-#   return "{0}:{1}:{2}" -f $hours, $minutes, $seconds
-# }
-# function make-report {
-#   Param([hashtable]$sum,
-#     [string] $reportName
-#   )
-#   mkdir -Path  "reports" -ErrorAction SilentlyContinue | Out-Null
-#   if (-not $reportName) { $reportName = "summary" }
-#   $reportPath = Join-Path -Path "reports" -ChildPath (".\{0}{1}_{2}.csv" -f $reportName, $Start, $End)
-#   if (Test-Path $reportPath) {
-#     Remove-Item -Force -Path $reportPath
-#   }
-#   New-Item -Path $reportPath -ErrorAction "SilentlyContinue" | Out-Null
-#   '"Project","Hours","Decimal"' | Add-Content -Encoding utf8BOM -Path $reportPath
-#   foreach ($i in $sum.GetEnumerator()) {
-#     "`"{0}`",`"{1}`",`"{2}`"" -f $i.Name, (get-hours-from-decimal -num $i.Value), $i.Value | Add-Content -Encoding utf8BOM -Path $reportPath
-#   }
-# }
-function main {
-  foreach ($ws in $wpId.GetEnumerator()) { 
-    $allProjectJson = get-report -ws $ws.Value
-    #$projectID = @{}; foreach ($i in ($allProjectJson | ConvertFrom-Json)) { $projectID[$i.Name] = $i.ID }
-    foreach($i in ($allProjectJson | ConvertFrom-Json)) {$i.timeentries}
 
-  }
+function seconds-to-hours {
+    Param([double] $num)
+    return $num / 3600
+}
+function make-report {
+    Param(
+        [string] $projectName,
+        [string] $userName,
+        [string] $description,
+        $timeInterval
+    )
+    if (-not $projectName) {$projectName = "No project"}
+    $reportPath = (Join-Path -Path $pathToReports -ChildPath ("{0} {1}_{2}.csv" -f $projectName, $Start, $End))
+    New-Item -Path $reportPath -ErrorAction "SilentlyContinue" | Out-Null
+    # Make header
+    if (-not (Get-Content $reportPath -ErrorAction "SilentlyContinue").Count) { 
+        '"Name","Description","From","To","Hours","Duration in seconds"'
+      | Add-Content -Encoding utf8BOM -Path $reportPath 
+    }
+  
+    "`"{0}`",`"{1}`",`"{2}`",`"{3}`",`"{4}`",`"{5}`"" `
+        -f $userName, $description, $timeInterval.start, $timeInterval.end, 
+            (seconds-to-hours -num $timeInterval.duration), $timeInterval.duration  | Add-Content -Encoding utf8BOM -Path $reportPath
+}
+function main {
+    foreach ($ws in $wpId.GetEnumerator()) { 
+        $allProjectJson = get-report -ws $ws.Value
+        #$projectID = @{}; foreach ($i in ($allProjectJson | ConvertFrom-Json)) { $projectID[$i.Name] = $i.ID }
+        foreach ($i in ($allProjectJson | ConvertFrom-Json)) {
+            $i.timeentries | ForEach-Object {
+                make-report -projectName $_.projectName -userName $_.userName `
+                    -description $_.description -timeInterval $_.timeInterval
+            }
+        }
+    }
 }
 main
